178c037a037b209de8e928c9892a5583e13d5ef5
Sam Potter <sfp@hush.ai>
2018-06-04 19:38:26 -0400

    continuing to redo octree

--- src/py/octree.py
+++ src/py/octree.py
@@ -23,9 +23,7 @@ def get_offset(inds):
     return np.array([
         bool2off(vec) for vec in list(zip(*[splits[k] for k in inds]))])
 
-_elt_dtype = np.dtype([('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('ind', 'i4')])
-
-def build_octree_level(X, inds):
+def get_octree_children(X, inds, l):
     '''Builds a level of an octree heap. The input `X' is an Nx3 numpy
 array, where each row corresponds to a point in 3-space. The function
 returns a length 8 list where each element is either None (indicating
@@ -42,68 +40,38 @@ subarray of `X'.
         np.array(X[:, 1] > (yext[1] + yext[0])/2, dtype=np.uint8),
         np.array(X[:, 2] > (zext[1] + zext[0])/2, dtype=np.uint8)])
 
-    octs = 4*splits[:, 0] + 2*splits[:, 1] + splits[:, 2]
+    oct_inds = 4*splits[:, 0] + 2*splits[:, 1] + splits[:, 2]
 
     for i in range(8):
-        sel = np.where(i == octs)[0]
+        sel = np.where(i == oct_inds)[0]
         if len(sel) == 0:
-            level[i] = (None, None)
+            level[i] = None
         else:
-            level[i] = (X[sel, :], inds[sel])
+            level[i] = OctreeNode(X[sel, :], inds=inds[sel], l=l)
 
     return level
 
-def build_octree_heap(X0, lmax):
-    '''Build the heap for the octree.'''
-    assert(lmax > 0)
+class OctreeNode(object):
+    def __init__(self, X, inds=None, l=0):
+        if inds is None:
+            inds = np.arange(X.shape[0], dtype=np.int32)
+        if l == 0:
+            self._children = None
+            self._X = X
+            self._inds = inds
+        else:
+            self._children = get_octree_children(X, inds, l - 1)
+            self._X = None
+            self._inds = None
+            
+        self._extent = util.get_extent(X)
 
-    nrows = X0.shape[0]
-
-    heap = build_octree_level(X0, np.arange(nrows, dtype=np.int32))
-
-    def build_octree_heap_rec(level, l):
-        if l >= lmax:
-            return
-        for i, (X, inds) in enumerate(level):
-            if X is None:
-                continue
-            level[i] = build_octree_level(X, inds)
-            build_octree_heap_rec(level[i], l + 1)
-
-    build_octree_heap_rec(heap, 1)
-
-    return heap
-
-def leaves(h, inds=[], yield_inds=False):
-    for i, cell in enumerate(h):
-        if type(cell) == list:
-            yield from leaves(cell, inds + [i], yield_inds)
-        elif type(cell) == np.ndarray:
-            if yield_inds:
-                yield inds + [i]
-            else:
-                yield cell
-
-class Octree(object):
-    def __init__(self, data, lmax):
-        self._data = data
-        self._heap = build_octree_heap(self._data, lmax)
-        self._lmax = lmax
-
-    def __iter__(self):
-        return leaves(self._heap)
-
-    def __getitem__(self, key):
-        if type(key) == int:
-            key = (key,)
-        assert(type(key) == tuple)
-        assert(len(key) == self._lmax)
-        i = 0
-        node = self._heap
-        while i < self._lmax and self._heap[key[i]] is not None:
-            node = node[key[i]]
-            i = i + 1
-        return node
-
-    def leaves(self, yield_inds=False):
-        return leaves(self._heap, yield_inds=yield_inds)
+    def __getitem__(self, *args):
+        print(args)
+        if type(args[0]) is int:
+            return self._children[args[0]]
+        else:
+            inds = args[0]
+            if len(inds) == 1:
+                return self._children[inds[0]]
+            return self._children[inds[0]].__getitem__(inds[1:])
