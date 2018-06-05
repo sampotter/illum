a5aa51b4c6f38d911d4c75aa70a2b1fabca3c1ae
Sam Potter <sfp@hush.ai>
2018-06-05 16:54:44 -0400

    continuing to work on the octree

--- src/py/octree.py
+++ src/py/octree.py
@@ -60,29 +60,73 @@ class Triangulation(object):
         self._faces = faces
         self._normals = normals
         self._albedos = albedos
-        self._inds = np.arange(self.num_faces()) if inds is None else inds
+        self._inds = np.arange(self.num_faces) if inds is None else inds
 
-    def num_faces(self):
+    def get_verts(self):
+        return self._verts
+
+    verts = property(get_verts)
+
+    def get_faces(self):
+        return self._faces
+
+    faces = property(get_faces)
+
+    def get_normals(self):
+        return self._normals
+
+    normals = property(get_normals)
+
+    def get_albedos(self):
+        return self._albedos
+
+    albedos = property(get_albedos)
+
+    def get_inds(self):
+        return self._inds
+
+    inds = property(get_inds)
+
+    def get_num_faces(self):
         return self._faces.shape[0]
 
-    def num_verts(self):
+    num_faces = property(get_num_faces)
+
+    def get_num_verts(self):
         return self._verts.shape[0]
+
+    num_verts = property(get_num_verts)
 
     def incident_faces(self, inds, sel=None):
         # TODO: this isn't a great way to do this, but we'll use it
         # for now...
-        mask = np.zeros((self.num_faces(), 3), dtype=np.bool)
+        mask = np.zeros((self.num_faces, 3), dtype=np.bool)
         for ind in inds:
-            mask = np.logical_or(mask, self._faces == ind)
+            mask = np.logical_or(mask, self.faces == ind)
         mask = np.where(np.logical_or(mask[:, 0], mask[:, 1], mask[:, 2]))[0]
         if sel is None:
             sel = inds
         return Triangulation(
-            self._verts[sel],
-            self._faces[mask, :],
-            self._normals[sel],
-            self._albedos[sel],
+            self.verts[sel],
+            self.faces[mask, :],
+            self.normals[sel],
+            self.albedos[sel],
             inds=inds)
+
+    def faces_in_extent(self, extent):
+        faces = []
+        for face in self.faces:
+            v = self.verts[face]
+            if any(np.all(np.logical_and(l <= v[:, i], v[:, i] <= r))
+                   for i, (l, r) in enumerate(extent)):
+                faces.append(face)
+        return np.array(faces, dtype=np.int32)
+
+def ray_intersects_octree_node(p, n, node):
+    if node is None:
+        return False
+    else:
+        return util.ray_intersects_box(p, n, *node.extent)
 
 class OctreeNode(object):
     def __init__(self, parent, tri, l=0):
@@ -95,16 +139,23 @@ class OctreeNode(object):
             self._tri = None
         self._extent = util.get_extent(tri._verts)
 
+    def get_extent(self):
+        return self._extent
+
+    extent = property(get_extent)
+
     def is_leaf_node(self):
         return self._children is None
 
-    def leaves(self):
+    def get_leaves(self):
         if self.is_leaf_node():
             yield self
         else:
             for child in self._children:
                 if child is not None:
                     yield from child.leaves()
+
+    leaves = property(get_leaves)
 
     def __getitem__(self, *args):
         if type(args[0]) is int:
@@ -116,16 +167,25 @@ class OctreeNode(object):
             return self._children[inds[0]].__getitem__(inds[1:])
 
     def ray_tri_intersections(self, p, n, tri):
-        if ray_intersects_box(p, n, *self._extent):
+        if util.ray_intersects_box(p, n, *self._extent):
             if self.is_leaf_node():
                 for face in self._tri._faces:
-                    t = ray_tri_intersection(p, n, *tri._verts[face])
+                    t = util.ray_tri_intersection(p, n, *tri._verts[face])
                     if t >= 0:
                         yield face
             else:
                 for node in self._children:
                     if node is not None:
                         yield from node.ray_tri_intersections(p, n, tri)
+
+    def get_nodes_containing_vertex(self, p):
+        if extent_contains_point(p, *self.extent):
+            if self.is_leaf_node():
+                yield self
+            else:
+                for child in self._children:
+                    if child is not None:
+                        yield from child.get_node_containing_vertex(p)
 
 def default_lmax(tri):
     nfaces = tri._faces.shape[0]
@@ -137,11 +197,31 @@ class Octree(object):
         self._lmax = default_lmax(tri) if lmax is None else lmax
         self._root = OctreeNode(None, tri, l=self._lmax)
 
-    def leaves(self):
+    def get_extent(self):
+        return self._root._extent
+
+    extent = property(get_extent)
+
+    def get_leaves(self):
         return self._root.leaves()
+
+    leaves = property(get_leaves)
+
+    def get_center(self):
+        return np.array([np.mean(ext) for ext in self.extent])
+
+    center = property(get_center)
+
+    def get_corners(self):
+        return \
+            np.array([ext[0] for ext in octree.extent]), \
+            np.array([ext[1] for ext in octree.extent])
 
     def __getitem__(self, *args):
         return self._root.__getitem__(*args)
 
     def ray_tri_intersections(self, p, n):
         return self._root.ray_tri_intersections(p, n, self._tri)
+
+    def get_nodes_containing_vertex(self, p):
+        return self._root.get_node_containings_vertex(p)
