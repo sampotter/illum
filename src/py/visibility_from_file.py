e0e0272967b5526fed3e4dc129e320953c84ec08
Sam Potter <sfp@hush.ai>
2018-06-08 10:35:27 -0400

    add #vis visualization script

---
+++ src/py/visibility_from_file.py
@@ -0,0 +1,31 @@
+import scipy.sparse
+
+from mayavi import mlab
+from obj import *
+
+path = '../../data/vis1.txt'
+
+f = open(path, 'r')
+
+S_dok = scipy.sparse.dok_matrix((50000, 50000), np.bool)
+
+for line in f:
+    i, j = tuple(map(int, line.split(',')))
+    S_dok[i, j] = True
+    S_dok[j, i] = True
+
+S_csr = S_dok.tocsr()
+
+nvis = np.array(np.sum(S_csr, 1)).flatten()
+nvis /= nvis.max()
+
+path = '../../data/Phobos_Ernst_decimated50k.obj'
+verts, face, norms, albs = readobj(path)
+x, y, z = v.T
+
+mesh = mlab.triangular_mesh(x, y, z, face, representation='wireframe', opacity=0)
+mesh.mlab_source.dataset.cell_data.scalars = nvis
+mesh.mlab_source.dataset.cell_data.scalars.name = 'nvis'
+mesh.mlab_source.update()
+mesh2 = mlab.pipeline.set_active_attribute(mesh, cell_scalars='nvis')
+surf = mlab.pipeline.surface(mesh2)
