65fc6272cc2cc2efd236eae0fb968fffa18afa26
Sam Potter <sfp@umiacs.umd.edu>
2018-06-29 16:34:13 -0400

    updated mpitest.sh

--- slurm/mpitest.sh
+++ slurm/mpitest.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 #SBATCH --job-name=mpitest
-#SBATCH --mpi=openmpi
-#SBATCH --nodes=4
-#SBATCH --ntasks=8
-#SBATCH --ntasks-per-node=2
+#SBATCH --account=scavenger
+#SBATCH --partition=scavenger
+#SBATCH --nodes=2
+#SBATCH --ntasks=32
+#SBATCH --ntasks-per-node=16
 #SBATCH --ntasks-per-socket=1
 #SBATCH --time=08:00:00
 
@@ -12,4 +13,4 @@ source ../modules.sh
 ILLUM_CLI=../src/cpp/build/Release/illum_cli
 OBJ_FILE=../../data/SHAPE0.OBJ
 
-mpirun -np 4 $ILLUM_CLI horizons -p $OBJ_FILE
+srun --mpi=openmpi $ILLUM_CLI horizons -p $OBJ_FILE
