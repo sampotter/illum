54583e78778083597ca7547b356f76c74c5d84a3
Sam Potter <sfp@hush.ai>
2018-07-27 15:24:41 -0400

    add arma.tofile

--- src/py/arma.py
+++ src/py/arma.py
@@ -6,9 +6,22 @@ def fromfile(path, dtype=np.float64):
         line = f.readline()
         # TODO: this first line also contains information about the dtype
         if line[:4] != b'ARMA':
-            raise Exception("file %s isn't in Armadillo binary format" % path)
+            return np.fromfile(path, dtype)
         dims = tuple(map(int, f.readline().split()))
         return np.fromfile(f, dtype=dtype).reshape(*dims)
+
+def tofile(X, path):
+    if X.dtype != np.float64:
+        raise Exception('Only implemented for float64 so far')
+    with open(path, 'wb') as f:
+        f.write(b'ARMA_MAT_BIN_FN008\n')
+        if X.ndim == 1:
+            f.write(b'%d\n' % X.size)
+        elif X.ndim == 2:
+            f.write(b'%d %d\n' % X.shape)
+        else:
+            raise Exception('Only X.ndim == 1 and X.ndim == 2 allowed')
+        X.tofile(f)
 
 def loadcoo(path, dtype=np.float64, index_dtype=np.uint64):
     rows, cols, vals = [], [], []
