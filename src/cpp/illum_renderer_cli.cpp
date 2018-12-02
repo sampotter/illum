d0a25362ec91c1acb6fd7ca0e26117b2f52c2f53
Sam Potter <sfp@umiacs.umd.edu>
2018-12-01 19:30:31 -0500

    improvements to renderer code

--- src/cpp/illum_renderer_cli.cpp
+++ src/cpp/illum_renderer_cli.cpp
@@ -19,11 +19,19 @@ template <class T> using opt_t = boost::optional<T>;
 
 constexpr double PI = 3.141592653589793;
 
-enum class mode_e {ORTHO};
+enum class mode_e {
+  EQUAL_AREA_CYLINDRICAL,
+  ORTHOGRAPHIC
+};
 
 mode_e string_to_mode(std::string const & mode_string) {
-  if (mode_string == "ortho") return mode_e::ORTHO;
-  else throw std::invalid_argument("invalid mode: " + mode_string);
+  if (mode_string == "orthographic") {
+    return mode_e::ORTHOGRAPHIC;
+  } else if (mode_string == "equal_area_cylindrical") {
+    return mode_e::EQUAL_AREA_CYLINDRICAL;
+  } else {
+    throw std::invalid_argument("invalid mode: " + mode_string);
+  }
 }
 
 int main(int argc, char * argv[])
@@ -33,99 +41,150 @@ int main(int argc, char * argv[])
   Options options(argv[0], "TODO");
   
   options.add_options()
-    ("h,help", "Display usage")
-    ("obj_path", "OBJ file path", value<std::string>())
-    ("d,data_path", "Path of data to render", value<std::string>())
-    ("i,img_path", "Output file path",
-     value<std::string>()->default_value("img.png"))
-    ("shape_index", "OBJ file shape index", value<int>()->default_value("0"))
-    ("l,layer", "Which layer (column) of the input data to plot",
-     value<arma::uword>()->default_value("0"))
-    ("W,width", "Image width [px]", value<int>()->default_value("512"))
-    ("H,height", "Image height [px]", value<int>()->default_value("512"))
-    ("m,min_value", "Minimum value for plot range", value<double>())
-    ("M,max_value", "Maximum value for plot range", value<double>())
-    ("az_cam", "Camera azimuth [deg]", value<double>()->default_value("0"))
-    ("el_cam", "Camera elevation [deg]", value<double>()->default_value("0"))
-    ("r_cam", "Distance of camera from model centroid [km]",
-     value<double>()->default_value("0"))
-    ("mode", "Rendering mode", value<std::string>()->default_value("ortho"))
-    ("colormap", "Color map to use to render",
-     value<std::string>()->default_value("grayscale"))
-    ("transpose",
-     "Transpose data first (use to plot rows instead of columns using -l",
-     value<bool>()->default_value("false"))
+    (
+      "h,help",
+      "Display usage"
+    )
+    (
+      "path",
+      "path to OBJ file",
+      value<std::string>()
+    )
+    (
+      "data",
+      "path of data to render",
+      value<std::string>()
+    )
+    (
+      "img",
+      "filename of saved PNG image",
+      value<std::string>()->default_value("img.png")
+    )
+    (
+      "shape",
+      "shape index of mesh in OBJ file",
+      value<int>()->default_value("0")
+    )
+    (
+      "L,layer",
+      "layer (column) of input data to plot",
+      value<arma::uword>()->default_value("0")
+    )
+    (
+      "W,width",
+      "image width [px]",
+      value<int>()->default_value("512")
+    )
+    (
+      "H,height",
+      "image height [px]",
+      value<int>()->default_value("512")
+    )
+    (
+      "m,minval",
+      "minimum value for plot range",
+      value<double>()
+    )
+    (
+      "M,maxval",
+      "maximum value for plot range",
+      value<double>()
+    )
+    (
+      "az",
+      "Camera azimuth [deg]",
+      value<double>()->default_value("0")
+    )
+    (
+      "el",
+      "camera elevation [deg]",
+      value<double>()->default_value("0")
+    )
+    (
+      "rad",
+      "distance of camera from model centroid [km]",
+      value<double>()->default_value("0")
+    )
+    (
+      "mode",
+      "rendering mode or map projection",
+      value<std::string>()->default_value("orthographic")
+    )
+    (
+      "cmap",
+      "color map to use to render",
+      value<std::string>()->default_value("grayscale")
+    )
+    (
+      "transpose",
+      "transpose data first (use to plot rows instead of columns)",
+      value<bool>()->default_value("false")
+    )
     ;
 
-  options.parse_positional({"obj_path"});
-
   auto args = options.parse(argc, argv);
-  if (args["help"].as<bool>()) {
+  if (argc == 1 || args["help"].as<bool>()) {
     std::cout << options.help() << std::endl;
     std::exit(EXIT_SUCCESS);
   }
 
-  std::string obj_path = args["obj_path"].as<std::string>();
+  std::string path = args["path"].as<std::string>();
 
-  opt_t<std::string> data_path;
-  if (args["data_path"].count() > 0) {
-    data_path = args["data_path"].as<std::string>();
+  opt_t<std::string> data_str;
+  if (args["data"].count() > 0) {
+    data_str = args["data"].as<std::string>();
   }
 
-  std::string img_path = args["img_path"].as<std::string>();
+  std::string img_str = args["img"].as<std::string>();
 
-  int shape_index = args["shape_index"].as<int>();
+  int shape = args["shape"].as<int>();
   arma::uword layer = args["layer"].as<arma::uword>();
   int width = args["width"].as<int>();
   int height = args["height"].as<int>();
-  double az_cam = args["az_cam"].as<double>();
-  double el_cam = args["el_cam"].as<double>();
-  double r_cam = args["r_cam"].as<double>();
+  double az = args["az"].as<double>();
+  double el = args["el"].as<double>();
+  double rad = args["rad"].as<double>();
   mode_e mode = string_to_mode(args["mode"].as<std::string>());
-  std::string colormap = args["colormap"].as<std::string>();
+  std::string cmap_str = args["cmap"].as<std::string>();
   
-  opt_t<double> min_value;
-  if (args["min_value"].count() > 0) {
-    min_value = args["min_value"].as<double>();
+  opt_t<double> minval;
+  if (args["minval"].count() > 0) {
+    minval = args["minval"].as<double>();
   }
 
-  opt_t<double> max_value;
-  if (args["max_value"].count() > 0) {
-    max_value = args["max_value"].as<double>();
+  opt_t<double> maxval;
+  if (args["maxval"].count() > 0) {
+    maxval = args["maxval"].as<double>();
   }
 
-  if (args["r_cam"].count() > 0) {
-    if (mode == mode_e::ORTHO) {
-      std::cerr << "setting r_cam while mode is ortho is invalid" << std::endl;
+  if (args["rad"].count() > 0) {
+    if (mode == mode_e::ORTHOGRAPHIC) {
+      std::cerr << "can't set radius in orthographic mode" << std::endl;
       std::exit(EXIT_FAILURE);
     } else {
-      r_cam = args["r_cam"].as<double>();
+      rad = args["rad"].as<double>();
     }
   }
 
-  if (!data_path) {
-    throw std::runtime_error("plot radius not implemented yet");
-  }
-
   arma::mat data_matrix;
-  data_matrix.load(*data_path);
+  data_matrix.load(*data_str);
   if (args["transpose"].as<bool>()) {
     data_matrix = data_matrix.t();
   }
 
   arma::vec data = data_matrix.col(layer);
-  if (min_value) {
+  if (minval) {
     for (arma::uword i = 0; i < data.n_elem; ++i) {
-      data(i) = std::max(*min_value, data(i));
+      data(i) = std::max(*minval, data(i));
     }
   }
-  if (max_value) {
+  if (maxval) {
     for (arma::uword i = 0; i < data.n_elem; ++i) {
-      data(i) = std::min(*max_value, data(i));
+      data(i) = std::min(*maxval, data(i));
     }
   }
   
-  auto objects = obj_util::get_objects(obj_path.c_str(), shape_index);
+  auto objects = obj_util::get_objects(path.c_str(), shape);
 
   if (data.n_elem != objects.size()) {
     throw std::runtime_error {"input data and mesh have incompatible sizes"};
@@ -143,40 +202,40 @@ int main(int argc, char * argv[])
     r_model = fmax(r_model, r);
   }
 
-  if (mode == mode_e::ORTHO) {
-    r_cam = 1.1*r_model;
+  if (mode == mode_e::ORTHOGRAPHIC) {
+    rad = 1.1*r_model;
   }
 
   BVH bvh {&objects};
 
-  az_cam *= PI/180;
-  el_cam *= PI/180;
+  az *= PI/180;
+  el *= PI/180;
 
-  if (r_cam == 0) {
-    std::cerr << "r_cam is uninitialized somehow" << std::endl;
+  if (rad == 0) {
+    std::cerr << "rad is uninitialized somehow" << std::endl;
     std::exit(EXIT_FAILURE);
   }
 
   Vector3 p_cam {
-    static_cast<float>(r_cam*std::cos(az_cam)*std::sin(el_cam)),
-    static_cast<float>(r_cam*std::sin(az_cam)*std::sin(el_cam)),
-    static_cast<float>(r_cam*std::cos(el_cam))
+    static_cast<float>(rad*std::cos(az)*std::sin(el)),
+    static_cast<float>(rad*std::sin(az)*std::sin(el)),
+    static_cast<float>(rad*std::cos(el))
   };
 
   Vector3 n_eye = -normalize(p_cam);
 
   p_cam = p_cam + c_model;
   
-  double el_up = el_cam - 90;
+  double el_up = el - 90;
   Vector3 n_up {
-    static_cast<float>(std::cos(az_cam)*std::sin(el_up)),
-    static_cast<float>(std::sin(az_cam)*std::sin(el_up)),
+    static_cast<float>(std::cos(az)*std::sin(el_up)),
+    static_cast<float>(std::sin(az)*std::sin(el_up)),
     static_cast<float>(std::cos(el_up))
   };
 
   Vector3 n_right = n_eye^n_up;
 
-  if (mode == mode_e::ORTHO) {
+  if (mode == mode_e::ORTHOGRAPHIC) {
     struct elt {
       elt(int i, int j, double value): i {i}, j {j}, value {value} {}
       int i, j;
@@ -190,10 +249,10 @@ int main(int argc, char * argv[])
     IntersectionInfo info;
     for (int i = 0; i < height; ++i) {
       double y_eye = 2*(1. - static_cast<double>(i)/(height - 1)) - 1;
-      y_eye *= r_cam;
+      y_eye *= rad;
       for (int j = 0; j < width; ++j) {
         double x_eye = 2*static_cast<double>(j)/(width - 1) - 1;
-        x_eye *= aspect*r_cam;
+        x_eye *= aspect*rad;
 
         Vector3 p_ray = p_cam + y_eye*n_up + x_eye*n_right;
         Ray ray(p_ray, n_eye);
@@ -204,31 +263,13 @@ int main(int argc, char * argv[])
       }
     }
 
-    double lo = min_value ? *min_value : data.min();
-    double hi = max_value ? *max_value : data.max();
+    double lo = minval ? *minval : data.min();
+    double hi = maxval ? *maxval : data.max();
 
     for (auto & elt: elts) {
       elt.value -= lo;
       elt.value /= hi - lo;
     }
-
-    // auto cmap = (uint8_t **) cet_CBD1;
-
-    // auto lerp = [] (double x, uint8_t lo, uint8_t hi) -> uint8_t {
-    //   return static_cast<uint8_t>(
-    //     (1 - x)*static_cast<double>(lo) + x*static_cast<double>(hi));
-    // };
-
-    // auto mapcolor = [&lerp, &cmap] (double x) -> png::rgb_pixel {
-    //   x = 255*std::clamp(x, 0., 1.);
-    //   auto i = static_cast<uint8_t>(std::floor(x));
-    //   auto frac = x - std::floor(x);
-    //   return {
-    //     lerp(frac, cmap[i][0], cmap[i + 1][0]),
-    //     lerp(frac, cmap[i][1], cmap[i + 1][1]),
-    //     lerp(frac, cmap[i][2], cmap[i + 1][2])
-    //   };
-    // };
     
     auto jet = [] (double x) -> png::rgb_pixel {
       double r = std::clamp(x < 0.7 ? 4*x - 1.5 : -4*x + 4.5, 0., 1.);
@@ -246,7 +287,7 @@ int main(int argc, char * argv[])
       return {byte, byte, byte};
     };
 
-    auto cmap = colormap == "grayscale" ? bw : jet;
+    auto cmap = cmap_str == "grayscale" ? bw : jet;
 
     png::image<png::rgb_pixel> img {
       static_cast<png::uint_32>(width),
@@ -257,6 +298,6 @@ int main(int argc, char * argv[])
       img.set_pixel(elt.j, elt.i, cmap(elt.value));
     }
 
-    img.write(img_path);
+    img.write(img_str);
   }
 }
